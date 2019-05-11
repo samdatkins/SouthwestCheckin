9f72fabf44aa9960e06e8290227d67ce41c5f125
sam.d.atkins <sam.d.atkins@gmail.com>
2019-05-11 10:54:13 -0700

    Now takes file full of reservations

--- checkin.py
+++ checkin.py
@@ -2,28 +2,27 @@
 """Southwest Checkin.
 
 Usage:
-  checkin.py CONFIRMATION_NUMBER FIRST_NAME LAST_NAME [--email=<email_addr> | --mobile=<phone_num>] [-v | --verbose]
+  checkin.py FILE_NAME [-v | --verbose]
   checkin.py (-h | --help)
   checkin.py --version
 
 Options:
   -h --help     Show this screen.
   -v --verbose  Show debugging information.
-  --email=<email_addr>  Email address where notification will be sent to.
-  --mobile=<phone_num>  Phone number where text notification will be sent to.
   --version     Show version.
 
 """
-from datetime import datetime
-from datetime import timedelta
+import sys
+from datetime import datetime, timedelta
+from math import trunc
+from threading import Thread
+from time import sleep, time
+
 from dateutil.parser import parse
 from docopt import docopt
-from math import trunc
 from pytz import utc
+
 from southwest import Reservation, openflights
-from threading import Thread
-import sys
-import time
 
 CHECKIN_EARLY_SECONDS = 5
 
@@ -34,19 +33,22 @@ def schedule_checkin(flight_time, reservation):
     # check to see if we need to sleep until 24 hours before flight
     if checkin_time > current_time:
         # calculate duration to sleep
-        delta = (checkin_time - current_time).total_seconds() - CHECKIN_EARLY_SECONDS
+        delta = (checkin_time - current_time).total_seconds() - \
+            CHECKIN_EARLY_SECONDS
         # pretty print our wait time
         m, s = divmod(delta, 60)
         h, m = divmod(m, 60)
-        print("Too early to check in.  Waiting {} hours, {} minutes, {} seconds".format(trunc(h), trunc(m), s))
-        time.sleep(delta)
+        print("Too early to check in.  Waiting {} hours, {} minutes, {} seconds".format(
+            trunc(h), trunc(m), s))
+        trusty_sleep(delta)
     data = reservation.checkin()
     for flight in data['flights']:
         for doc in flight['passengers']:
-            print("{} got {}{}!".format(doc['name'], doc['boardingGroup'], doc['boardingPosition']))
+            print("{} got {}{}!".format(
+                doc['name'], doc['boardingGroup'], doc['boardingPosition']))
 
 
-def auto_checkin(reservation_number, first_name, last_name, notify=[]):
+def _auto_checkin(reservation_number, first_name, last_name, notify=[]):
     r = Reservation(reservation_number, first_name, last_name, notify)
     body = r.lookup_existing_reservation()
 
@@ -59,18 +61,44 @@ def auto_checkin(reservation_number, first_name, last_name, notify=[]):
     # find all eligible legs for checkin
     for leg in body['bounds']:
         # calculate departure for this leg
-        airport = "{}, {}".format(leg['departureAirport']['name'], leg['departureAirport']['state'])
+        airport = "{}, {}".format(
+            leg['departureAirport']['name'], leg['departureAirport']['state'])
         takeoff = "{} {}".format(leg['departureDate'], leg['departureTime'])
-        airport_tz = openflights.timezone_for_airport(leg['departureAirport']['code'])
-        date = airport_tz.localize(datetime.strptime(takeoff, '%Y-%m-%d %H:%M'))
+        airport_tz = openflights.timezone_for_airport(
+            leg['departureAirport']['code'])
+        date = airport_tz.localize(
+            datetime.strptime(takeoff, '%Y-%m-%d %H:%M'))
         if date > now:
             # found a flight for checkin!
-            print("Flight information found, departing {} at {}".format(airport, date.strftime('%b %d %I:%M%p')))
+            print("Flight information found, departing {} at {}".format(
+                airport, date.strftime('%b %d %I:%M%p')))
             # Checkin with a thread
             t = Thread(target=schedule_checkin, args=(date, r))
             t.daemon = True
             t.start()
             threads.append(t)
+
+    return threads
+
+
+def batch_auto_checkin(file_name):
+    threads = []
+    with open(file_name) as file:
+        content = file.readlines()
+
+    lines = [x.strip() for x in content]
+    for line in lines:
+        try:
+            [reservation_number, first_name, last_name, email] = line.split()
+        except ValueError:
+            print(f'Could not parse line {line}')
+            continue
+
+        # build out notifications
+        notify = [{'mediaType': 'EMAIL', 'emailAddress': email}]
+        new_threads = _auto_checkin(reservation_number,
+                                    first_name, last_name, notify)
+        threads.extend(new_threads)
 
     # cleanup threads while handling Ctrl+C
     while True:
@@ -82,25 +110,21 @@ def auto_checkin(reservation_number, first_name, last_name, notify=[]):
                 threads.remove(t)
                 break
 
+# sleep can't handle huge ints, so break it up in to smaller increments
+MAX_SLEEP = 10000
+def trusty_sleep(n):
+    start = time()
+    while (time() - start < n):
+        time_remaining = n - (time() - start)
+        sleep(min(time_remaining, MAX_SLEEP))
 
 if __name__ == '__main__':
 
     arguments = docopt(__doc__, version='Southwest Checkin 0.2')
-    reservation_number = arguments['CONFIRMATION_NUMBER']
-    first_name = arguments['FIRST_NAME']
-    last_name = arguments['LAST_NAME']
-    email = arguments['--email']
-    mobile = arguments['--mobile']
-
-    # build out notifications
-    notifications = []
-    if email is not None:
-        notifications.append({'mediaType': 'EMAIL', 'emailAddress': email})
-    if mobile is not None:
-        notifications.append({'mediaType': 'SMS', 'phoneNumber': mobile})
+    file_name = arguments['FILE_NAME']
 
     try:
-        auto_checkin(reservation_number, first_name, last_name, notifications)
+        batch_auto_checkin(file_name)
     except KeyboardInterrupt:
         print("Ctrl+C detected, canceling checkin")
         sys.exit()
